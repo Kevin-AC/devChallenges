23fbb740fe34825865cd1ade115532d40f0426fa
Kevin-AC <kevin.argumedo@gmail.com>
2023-07-26 20:57:56 -0500

    feat: add Router an styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import { CardAuthor } from './components/CardAuthor'
 import { Footer } from './components/Footer'
 import { Quote } from './components/Quote'
 import { useEffect, useState } from 'react'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { AuthorQuote } from './components/AuthorQuotes'
 const API = 'https://quote-garden.onrender.com/api/v3/quotes/random'
 function App () {
-  const [quote, setQuote] = useState()
+  const [quote, setQuote] = useState('')
   useEffect(() => {
     fetch(API)
       .then(res => res.json())
@@ -18,21 +20,40 @@ function App () {
       )
   }, [])
 
+  const handleClick = () => {
+    fetch(API)
+      .then(res => res.json())
+      .then(response => {
+        const data = response.data[0]
+        setQuote(data)
+      }
+      )
+  }
   return (
     <div className='w-screen p-9 flex flex-col justify-center items-center gap-8 min-h-screen bg-bg  font-Raleway  relative'>
       <header className='w-auto h-auto absolute top-[30px] right-[30px] '>
-        <button className='flex items-center justify-center gap-4'>
+        <button onClick={handleClick} className='flex items-center justify-center gap-4'>
           <p className='text-lg text-primary font-medium'>randon</p>
           <span className="material-symbols-outlined text-primary ">sync</span>
         </button>
       </header>
-      <main>
-        <Quote quoteText={quote.quoteText}/>
-        <CardAuthor
-          quoteAuthor={quote.quoteAuthor}
-          quoteGenre={quote.quoteGenre}
-        />
-      </main>
+      <Router>
+        <Routes>
+          <Route path='/' element={
+          <main className='flex flex-col gap-10'>
+              <Quote quoteText={quote.quoteText} />
+              <CardAuthor
+                quoteAuthor={quote.quoteAuthor}
+                quoteGenre={quote.quoteGenre}
+              />
+          </main>
+        }/>
+        <Route path='/Author' element={
+          <AuthorQuote quoteAuthor={quote.quoteAuthor}/>
+        } />
+        </Routes>
+      </Router>
+
       <Footer/>
     </div>
 
--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -3,7 +3,7 @@ export function Quote ({ quoteText }) {
   return (
     <section className='w-auto h-auto flex flex-col items-center gap-10'>
       <span className='w-2 h-[301px] bg-decoration absolute left-[220px]'></span>
-      <article className='w-[614px] h-[301px]' >
+      <article className='w-[614px] min-h-[301px]' >
         <p className='text-black text-4xl font-medium'>{quoteText}</p>
       </article>
     </section>
