e3ffe8b919e471f7eb2f44e3a24a87a7a66d0ec3
Kevin-AC <kevin.argumedo@gmail.com>
2023-07-26 20:56:43 -0500

    feat: all author quotes

--- src/components/AuthorQuotes.jsx
+++ src/components/AuthorQuotes.jsx
@@ -1,7 +1,28 @@
-export function AuthorQuote () {
+import { useEffect, useState } from 'react'
+import { Quote } from './Quote'
+
+/* eslint-disable react/prop-types */
+export function AuthorQuote ({ quoteAuthor }) {
+  const [quote, setQuote] = useState([])
+  useEffect(() => {
+    fetch(`https://quote-garden.onrender.com/api/v3/quotes?author=${quoteAuthor}`)
+      .then(res => res.json())
+      .then(response => {
+        setQuote(response.data)
+        console.log(response.data)
+      })
+  }, [quoteAuthor])
   return (
-   <section>
-    <h2></h2>
+   <section className='flex flex-col gap-20 mb-10'>
+      <h2 className='font-bold text-4xl mt-10 text-card'>{quoteAuthor}</h2>
+
+      {quote.map((data) => (
+      <Quote key={data._id}
+          quoteText={data.quoteText}
+      />)
+
+      )}
+
    </section>
   )
 }
