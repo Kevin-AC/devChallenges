e675cd67d877b7d4e652daa8dc482140b0035f83
Kevin-AC <kevin.argumedo@gmail.com>
2023-08-08 20:05:00 -0500

    fix: call Current Weather Data

--- src/services/Weather.jsx
+++ src/services/Weather.jsx
@@ -1,10 +1,9 @@
-const LOCAL_WEATHER_API = 'Api/Apartado.json'
+// const LOCAL_WEATHER_API = 'Api/Apartado.json'
 
 export const getWeather = (latitude, longitude) => {
   if (latitude && longitude) {
-    //  console.log(latitude, longitude)
-    // const WEATHER_API = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=0a24c3c97422d75fef0a7cd0b54595d4`
-    return fetch(LOCAL_WEATHER_API)
+    const WEATHER_API = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=0a24c3c97422d75fef0a7cd0b54595d4`
+    return fetch(WEATHER_API)
       .then(resp => resp.json())
       .then(data => {
         const respose = data
